135425176bc0afbb9242d6f93d645f3d846e91c9
WEICHUANJUN <weichuanjun0528@gmail.com>
2021-02-18 11:48:40 +0900

    init

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
 
-func indexHandler(wi http.HandlerFunc, req *http.Request) {
-	fmt.Fprint(w, "URL path is %q/n", req.URL.Path)
+// handler echoes r.URL.Path
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+}
+
+// handler echoes r.URL.Header
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
 }
