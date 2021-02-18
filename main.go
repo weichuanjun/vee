e1ffe7dc0dfb98de5fd3dea69e68b27bd408ae0f
WEICHUANJUN <weichuanjun0528@gmail.com>
2021-02-18 11:36:24 +0900

    init

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,4 +10,8 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/hello", helloHandler)
 	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func indexHandler(wi http.HandlerFunc, req *http.Request) {
+	fmt.Fprint(w, "URL path is %q/n", req.URL.Path)
 }
