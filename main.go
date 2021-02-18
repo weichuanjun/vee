042139670918b6bb8da6147db4cd8bd621ff4c3e
WEICHUANJUN <weichuanjun0528@gmail.com>
2021-02-18 11:31:19 +0900

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
