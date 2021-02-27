bdd4cdbbca0bf00dd3e17a94ee1be7d4b8b927c3
WEICHUANJUN <weichuanjun0528@gmail.com>
2021-02-27 18:58:20 +0900

    context/router

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
 	"vee"
 )
 
 func main() {
 	r := vee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *vee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *vee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.POST("/login", func(c *vee.Context) {
+		c.JSON(http.StatusOK, vee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
